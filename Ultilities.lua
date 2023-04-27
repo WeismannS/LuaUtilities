dcfbbfadc2a490e97599587153b20fd1ddace8f3
Weismann <60429301+WeismannS@users.noreply.github.com>
2023-04-27 00:12:50 +0000

    Added :slice()

--- Ultilities.lua
+++ Ultilities.lua
@@ -1,126 +1,137 @@
 util = table
 function T(t)
-    return setmetatable(t, {__index = util})
+     return setmetatable(t, {__index = util})
 end
 local _ = setreadonly and setreadonly(util,false)
 function util.map(self,func)
-    local newT = T{}
-    for i,v in pairs(self) do
-        newT[i] = func(v)
-    end
-    return newT
+     local newT = T{}
+     for i,v in pairs(self) do
+          newT[i] = func(v)
+     end
+     return newT
 end
 
 function util.concat(self,...)
-    local newT = T{};
-    local tables = {...}
-    for i,v in pairs(self) do
-        newT[i] = v
-    end
-    for i,t in pairs(tables) do
-        for i2,v in pairs(t) do
-            util.insert(newT,v)
-        end
-    end
-    return newT
+     local newT = T{};
+     local tables = {...}
+     for i,v in pairs(self) do
+          newT[i] = v
+     end
+     for i,t in pairs(tables) do
+          for i2,v in pairs(t) do
+               util.insert(newT,v)
+          end
+     end
+     return newT
 end
 
 function util.foreach(self,func)
-    for i,v in pairs(self) do
-        func(i,v) 
-    end
+     for i,v in pairs(self) do
+          func(i,v)
+     end
 end
 
 function util.find(self,func)
-    for i,v in pairs(self) do
-        if func(self[i],i) then
-            return v
-        end
-    end
+     for i,v in pairs(self) do
+          if func(self[i],i) then
+               return v
+          end
+     end
 end
 
 function util.reverse(self)
-    local newT = T{}
-    for i=#self,1,-1 do
-        table.insert(newT,self[i])
-    end
-    return newT
+     local newT = T{}
+     for i=#self,1,-1 do
+          table.insert(newT,self[i])
+     end
+     return newT
 end
 
 function util.filter(self,func)
-    local newT = T{} 
-    for i,v in pairs(self) do
-        if func(v,i) then
-            table.insert(newT,v)    
-        end 
-    end
-    return newT
+     local newT = T{}
+     for i,v in pairs(self) do
+          if func(v,i) then
+               table.insert(newT,v)
+          end
+     end
+     return newT
 end
 
 function util.values(self,func)
-    local newT = T{}
-    function loop(t)
-        for i,v in pairs(t) do
-            if type(v) == "table" then
-                loop(v)
-            else 
-                  newT[i] = v
-            end
-        end
-    end
-    loop(self)
-    return newT
+     local newT = T{}
+     function loop(t)
+          for i,v in pairs(t) do
+               if type(v) == "table" then
+                    loop(v)
+               else
+                    newT[i] = v
+               end
+          end
+     end
+     loop(self)
+     return newT
 end
 
 function util.clone(t)
-  local newT = T{}
-  for i,v in pairs(t) do
-    if type(v) == "table" then
-      newT[i] = util.clone(v)
-    else
-      newT[i] = v
-    end
-  end
-  return newT
+     local newT = T{}
+     for i,v in pairs(t) do
+          if type(v) == "table" then
+               newT[i] = util.clone(v)
+          else
+               newT[i] = v
+          end
+     end
+     return newT
 end
 
 function printf(v)
-    local space = ""
+     local space = ""
 
-    local function printAll(v)
-        if type(v) ~= "table" then
-            print(space .. tostring(v).." ,")
-        else
-            print(space .. "{")
-            space = space .. " "
-            for i2, v2 in pairs(v) do
-                print(space .. tostring(i2) .. " = ")
-                printAll(v2)
-            end
-            space = space:sub(1, -3)
-            print(space .. "}")
-        end
-    end
-    printAll(v)
+     local function printAll(v)
+          if type(v) ~= "table" then
+               print(space .. tostring(v).." ,")
+          else
+               print(space .. "{")
+               space = space .. " "
+               for i2, v2 in pairs(v) do
+                    print(space .. tostring(i2) .. " = ")
+                    printAll(v2)
+               end
+               space = space:sub(1, -3)
+               print(space .. "}")
+          end
+     end
+     printAll(v)
 end
 
 function util.length(self)
-      local count = 0    
-      for i,v in pairs(self) do
-        count = count+1
-      end
-      return count
+     local count = 0
+     for i,v in pairs(self) do
+          count = count+1
+     end
+     return count
 end
 
-function string.split(self, delimiter)
-    local newT = {}
-    local from = 1
-    local delim_from, delim_to = string.find(self, delimiter, from)
-    while delim_from do
-        table.insert(newT, string.sub(self, from, delim_from - 1))
-        from = delim_to + 1
-        delim_from, delim_to = string.find(self, delimiter, from)
-    end
-    table.insert(newT,string.sub(self, from))
-    return newT
+function util.slice(self,Begin,End)
+     local newT = {}
+     local _End = End or #self
+     for i=Begin,_End do
+          util.insert(newT,self[i])
+     end
+     return newT
 end
+
+
+function string.split(self, delimiter)
+     local newT = {}
+     local from = 1
+     local delim_from, delim_to = string.find(self, delimiter, from)
+     while delim_from do
+          table.insert(newT, string.sub(self, from, delim_from - 1))
+          from = delim_to + 1
+          delim_from, delim_to = string.find(self, delimiter, from)
+     end
+     table.insert(newT,string.sub(self, from))
+     return newT
+end
+
